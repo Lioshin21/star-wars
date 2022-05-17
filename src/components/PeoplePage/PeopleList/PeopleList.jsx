41a9509b044a4c237301400e4d99cd9cc5b34a7b
Lioshin21 <makivskii99@gmail.com>
2022-05-17 09:58:26 +0300

    created ui component with storybook

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -3,27 +3,29 @@ import styles from "./PeopleList.module.css";
 
 import propTypes from "prop-types";
 
-const PeopleList = ({ people }) => {
+// Routes
 
+import { Link } from "react-router-dom";
+
+const PeopleList = ({ people }) => {
   return (
     <ul className={styles.list__container}>
       {people.map(({ id, name, img }) => {
         return (
           <li className={styles.list__item} key={id}>
-            <a href="#">
+            <Link to={`/people/${id}`}>
               <img className={styles.person__photo} src={img} alt={name} />
               <p>{name}</p>
-            </a>
+            </Link>
           </li>
         );
       })}
     </ul>
   );
-
 };
 
 PeopleList.propTypes = {
-  people: propTypes.array
-}
+  people: propTypes.array,
+};
 
 export default PeopleList;
