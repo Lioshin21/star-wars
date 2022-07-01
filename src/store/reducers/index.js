e49fd28623bd05ed04295c830f92c7e91df03ae8
Lioshin21 <makivskii99@gmail.com>
2022-07-01 11:59:12 +0300

    clear branch

--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import favoriteReducer from "./favoriteReducer";
-
-const rootReducer = combineReducers({
-    favoriteReducer
-})
-
-export default rootReducer
